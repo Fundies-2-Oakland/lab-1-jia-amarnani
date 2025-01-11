720d078a870bc274f5341a23342616ddef23eb33
Jia Amarnani <jiaamarnani@gmail.com>
2025-01-11 15:13:16 -0800

    Q1
    Made constructer and initialized variables.

--- Vector3D.java
+++ Vector3D.java
@@ -1,6 +1,21 @@
 public class Vector3D {
+
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("git test hi!");
+
+
+
     }
+
+    double x;
+    double y;
+    double z;
+
+    public Vector3D(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    
+
+
 }
